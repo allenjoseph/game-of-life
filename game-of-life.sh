ac1e5c49c824aa16226d8301108830f0a205eb5d
Allen Joseph <avelasco@belatrixsf.com>
2017-05-02 16:33:06 -0500

    Added refactoring method: addCellsSeed

--- game-of-life.sh
+++ game-of-life.sh
@@ -7,83 +7,63 @@
 # declare: Declare variables and give them attributes
 # unset: Remove variable or function names
 
-GENERATION=-1
-CROWDING=0
+declare -i GENERATION=-1
+declare -i CROWDING=0
 
-GRID_START_ROW=2
-GRID_START_COL=0
+declare -i GRID_START_ROW=2
+declare -i GRID_START_COL=0
 
-GRID_HEIGHT=25
-GRID_WIDTH=100
+declare -i GRID_HEIGHT=25
+declare -i GRID_WIDTH=100
 
 declare -a CELLS
-CELL_INDEX=0
+declare -i CELL_INDEX=0
 
 declare -a TRASH
-TRASH_INDEX=0
+declare -i TRASH_INDEX=0
 
 declare -a NEWS
-NEWS_INDEX=0
+declare -i NEWS_INDEX=0
 
 declare -a NEIGHBUORS
-NEIGHBUORS_INDEX=0
+declare -i NEIGHBUORS_INDEX=0
 
 printLine() {
-    local col=0
-     while [ $col -lt $GRID_WIDTH ]; do
-        printf %s "-"
-        let col=col+1
-    done
+    printf "%*s\n" $(tput cols) | tr ' ' -
 }
 
 addCell() {
     # Parameters:
     # each cell contains "x|y|index" positions
-    local x=$1
-    local y=$2
+    x=$1
+    y=$2
 
     CELLS[$CELL_INDEX]="$x|$y|$CELL_INDEX"
     let CELL_INDEX++
 }
 
 addCellsSeed() {
-    # initials cells (seed.txt)
-    #    @@
-    #     @@
-    #     @
-
-    # Num of lines of seed
-    local lines=$(awk 'END { print NR }' seed.txt)
-
-    # Num characters of largest line
-    local cols=$(awk '{ if (length > L) {L=length} } END { print L }' seed.txt)
+    # Num of lines, cols of seed
+    declare -i lines=$(awk 'END { print NR }' seed.txt)
+    declare -i cols=$(awk '{ if (length > L) {L=length} } END { print L }' seed.txt)
 
     # center seed
-    local CENTERED_LEFT=$(((GRID_WIDTH - cols)/2))
-    local CENTERED_TOP=$(((GRID_HEIGHT - lines)/2))
+    declare -i CENTERED_LEFT=$(( (GRID_WIDTH - cols) / 2 ))
+    declare -i CENTERED_TOP=$(( (GRID_HEIGHT - lines) / 2 ))
 
-    # initial positions
-    local x=$CENTERED_LEFT
-    local y=$CENTERED_TOP
+    # initials cells is in seed.txt
+    # IFS: Internal Field Separator
+    # read -r: do not allow backslashes to escape any characters
+    while IFS= read -r line; do
 
-    # IFS is to count empty spaces
-    IFS=''
-    while read line; do
+        let CENTERED_TOP++
 
-        x=$CENTERED_LEFT; i=0
+        for(( i=0; i<=${#line}; i++ )); do
 
-        while [ $i -lt ${#line} ]; do
-
-            if [ ${line:$i:1} == "X" ]; then
-
-                addCell $x $y
+            if [[ "${line:$i:1}" == "X" ]]; then
+                addCell $(( CENTERED_LEFT+i )) $CENTERED_TOP
             fi
-
-            # increase the counters
-            let i++; let x++
         done
-
-         let y++
     done < seed.txt
 }
 
@@ -92,8 +72,8 @@ printGrid() {
     # add new cells
     for cell in ${NEWS[*]}; do
         IFS='|' read -ra positions <<< "$cell"
-        local x=${positions[0]}
-        local y=${positions[1]}
+        x=${positions[0]}
+        y=${positions[1]}
 
         findCell "$x|$y"
         if [ $? != 1 ]; then
@@ -109,8 +89,8 @@ printGrid() {
         if [ ${#cell} -gt 0 ]; then
 
             IFS='|' read -ra positions <<< "$cell"
-            local x=${positions[0]}
-            local y=${positions[1]}
+            x=${positions[0]}
+            y=${positions[1]}
 
             tput cup $y $x
             printf "X"
@@ -121,9 +101,9 @@ printGrid() {
     for cell in ${TRASH[*]}; do
 
         IFS='|' read -ra positions <<< "$cell"
-        local x=${positions[0]}
-        local y=${positions[1]}
-        local index=${positions[2]}
+        x=${positions[0]}
+        y=${positions[1]}
+        index=${positions[2]}
 
         unset CELLS[$index]
         tput cup $y $x
@@ -146,32 +126,32 @@ findNeighbuor() {
 evaluateNeighbuors() {
     # Parameters:
     # each cell contains "x|y" positions
-    local x=$1
-    local y=$2
+    x=$1
+    y=$2
 
     # u: up
     # d: down
-    local u="$x|$((y-1))"
-    local d="$x|$((y+1))"
+    u="$x|$((y-1))"
+    d="$x|$((y+1))"
 
     # l: left
     # lu: left up
     # ld: left down
-    local l="$((x-1))|$y"
-    local lu="$((x-1))|$((y-1))"
-    local ld="$((x-1))|$((y+1))"
+    l="$((x-1))|$y"
+    lu="$((x-1))|$((y-1))"
+    ld="$((x-1))|$((y+1))"
 
     # r: right
     # ru: right up
     # rd: right down
-    local r="$((x+1))|$y"
-    local ru="$((x+1))|$((y-1))"
-    local rd="$((x+1))|$((y+1))"
+    r="$((x+1))|$y"
+    ru="$((x+1))|$((y-1))"
+    rd="$((x+1))|$((y+1))"
 
     # index of cell
-    local index=$3
+    index=$3
 
-    local count=0
+    count=0
     for neighbuor in $u $ru $r $rd $d $ld $l $lu; do
         findCell $neighbuor
         if [ $? == 1 ]; then
@@ -211,23 +191,23 @@ evaluateLife() {
     for cell in ${CELLS[*]}; do
 
         IFS='|' read -ra positions <<< "$cell"
-        local x=${positions[0]}
-        local y=${positions[1]}
-        local index=${positions[2]}
+        x=${positions[0]}
+        y=${positions[1]}
+        index=${positions[2]}
 
         evaluateNeighbuors $x $y $index
     done
 
-    local temp_neighbuors=(${NEIGHBUORS[*]})
+    temp_neighbuors=(${NEIGHBUORS[*]})
     NEIGHBUORS=
     let NEIGHBUORS_INDEX=0
 
     for neighbuor in ${temp_neighbuors[*]}; do
 
         IFS='|' read -ra positions <<< "$neighbuor"
-        local x=${positions[0]}
-        local y=${positions[1]}
-        local index=-1
+        x=${positions[0]}
+        y=${positions[1]}
+        index=-1
 
         evaluateNeighbuors $x $y $index
     done
@@ -257,6 +237,10 @@ printf "\nPress 'n' to next generation \n"
 
 while :
 do
+    # read -s: do not echo input coming from a terminal.
+    # read -n: return after reading NCHARS characters rather than waiting
+    #          for a newline, but honor a delimiter if fewer than
+    #          NCHARS characters are read before the delimiter.
     read -s -n 1 key
     case "$key" in
         n) 
